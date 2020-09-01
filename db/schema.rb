edacadf627649db7acb364702555bf88cd9422de
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-31 21:45:07 -0600

    Update database with favorites table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200901014756) do
+ActiveRecord::Schema.define(version: 20200901033810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,16 @@ ActiveRecord::Schema.define(version: 20200901014756) do
     t.string "country"
     t.string "description"
     t.string "images"
-    t.boolean "favorite", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "breed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["breed_id"], name: "index_favorites_on_breed_id"
+  end
+
+  add_foreign_key "favorites", "breeds"
 end
