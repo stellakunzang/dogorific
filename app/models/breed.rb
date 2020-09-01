aa11b1b2fe42835209fb1f2a07f59e4f35e206dc
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-31 21:45:53 -0600

    Update models with relationships

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,7 +1,8 @@
 class Breed < ApplicationRecord
+  has_many :favorites 
   serialize :images, Array
   validates :name, presence: true
   validates :country, presence: true
   validates :description, presence: true
   validates :images, presence: true
-end 
+end
