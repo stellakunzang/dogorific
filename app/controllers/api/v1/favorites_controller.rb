7e66278b16d16c9d3baa8bca09927ba2cff75db2
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-01 14:06:01 -0600

    Return json for single favorite

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::FavoritesController < ApplicationController
   def index
     render json: FavoriteSerializer.new(Favorite.all)
   end
+
+  def show
+    render json: FavoriteSerializer.new(Favorite.find(params[:id]))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :breeds, only: [:index, :show]
-      resources :favorites, only: [:index]
+      resources :favorites, only: [:index, :show]
     end
   end
 end
