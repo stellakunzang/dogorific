3ff50ed149b6547942b4d83438721bbd677bf555
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-01 16:04:31 -0600

    Add create method to controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -7,4 +7,21 @@ class Api::V1::FavoritesController < ApplicationController
   def show
     render json: FavoriteSerializer.new(Favorite.find(params[:id]))
   end
+
+  def create
+    favorite = Breed.find(params[:breed]).favorites.create
+    if favorite.save
+      render json: ConfirmationSerializer.new(successfully_created_message).info
+    end
+  end
+
+  private
+
+  def successfully_created_message
+    "The breed was successfully added to the favorites list."
+  end
+
+  def successfully_deleted_message
+    "The favorite was successfully deleted, and will now longer appear on the favorites list."
+  end
 end
