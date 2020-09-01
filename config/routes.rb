b302095772248b091f8ff9ff392fe7de32531612
stellakunzang <stella.k.bonnie@gmail.com>
2020-09-01 16:04:02 -0600

    Add route for favorites post request

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :breeds, only: [:index, :show]
       resources :favorites, only: [:index, :show]
+      post 'favorites/add', to: 'favorites#create'
     end
   end
 end
